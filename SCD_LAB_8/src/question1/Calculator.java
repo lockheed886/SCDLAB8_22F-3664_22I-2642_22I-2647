4cf1a31fe0942717cbe6e4d2fc457c21856786ac
Waleed-Ahmad <i222647@nu.edu.pk>
2024-10-11 10:36:35 +0500

    i

--- SCD_LAB_8/src/question1/Calculator.java
+++ SCD_LAB_8/src/question1/Calculator.java
@@ -1,29 +1,45 @@
 package question1;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
 
-	public int addition(int num1, int num2)
-	{
-		return (num1+num2);
-	}
+    public int addition(int num1, int num2) {
+        return num1 + num2;
+    }
 
-	public static void main(String[] args) {
-		
-		Calculator cal =  new Calculator();
-		Scanner s = new Scanner(System.in);
-		
-		int n1 = 5, n2 = 10;
-		System.out.println("Enter the 1st number: ");
-		n1 = s.nextInt();
-		
-		System.out.println("Enter the 2nd number: ");
-		n2 = s.nextInt();
-		
-		int ans = cal.addition(n1, n2);
-		System.out.println("Ans: " + ans);
+    public int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
 
-	}
+    public int getValidInput(Scanner scanner, String prompt) {
+        int num;
+        while (true) {
+            System.out.print(prompt);
+            try {
+                num = scanner.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+                scanner.next();
+            }
+        }
+        return num;
+    }
 
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        Scanner s = new Scanner(System.in);
 
+        int n1 = cal.getValidInput(s, "Enter the 1st number: ");
+        int n2 = cal.getValidInput(s, "Enter the 2nd number: ");
+        
+        int additionResult = cal.addition(n1, n2);
+        System.out.println("Addition result: " + additionResult);
+
+        int multiplicationResult = cal.multiplication(n1, n2);
+        System.out.println("Multiplication result: " + multiplicationResult);
+
+        s.close();
+    }
 }
