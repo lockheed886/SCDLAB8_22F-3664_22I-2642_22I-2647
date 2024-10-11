8dd4a228aaff6dbdeb8aff27959fa2a05743b11d
Waleed-Ahmad <i222647@nu.edu.pk>
2024-10-11 10:21:05 +0500

    Input Validation

--- SCD_LAB_8/src/question1/Calculator.java
+++ SCD_LAB_8/src/question1/Calculator.java
@@ -1,9 +1,38 @@
 package question1;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Calculator {
 
-	public static void main(String[] args) {
-	
-	}
+    public int addition(int num1, int num2) {
+        return num1 + num2;
+    }
 
+    public int getValidInput(Scanner scanner, String prompt) {
+        int num;
+        while (true) {
+            System.out.print(prompt);
+            try {
+                num = scanner.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+                scanner.next(); 
+            }
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        Scanner s = new Scanner(System.in);
+
+        int n1 = cal.getValidInput(s, "Enter the 1st number: ");
+        int n2 = cal.getValidInput(s, "Enter the 2nd number: ");
+        
+        int ans = cal.addition(n1, n2);
+        System.out.println("Ans: " + ans);
+
+        s.close();
+    }
 }
