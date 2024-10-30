e8aa37d35a5c6f0c0ceab622735303299bafac4e
22F-3664 <f223664@cfd.nu.edu.pk>
2024-10-31 01:53:41 +0500

    Transliteration Use case aded
    I have merge the transliteration use case code in the existing project .The code's all functions is working properly.
    This my Usecase (transliteration) also require a JAR File icu4j-73.2 , which is
    necessary for run the code.

--- SCD_LAB_8/src/question1/Calculator.java
+++ SCD_LAB_8/src/question1/Calculator.java
@@ -1,28 +1,9 @@
 package question1;
 
-import java.util.Scanner;
-
 public class Calculator {
 
-   
-    public int multiplication(int a, int b) {
-        return a * b;
-    }
+	public static void main(String[] args) {
+	
+	}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-        
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
-        
-        Calculator calc = new Calculator();
-        int result = calc.multiplication(num1, num2);
-        
-        System.out.println("Multiplication result: " + result);
-        
-        scanner.close();
-    }
 }
